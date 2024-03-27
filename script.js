a04ba9adc88c3a9b0ff8f2d8e92e21cdeb45d502
der411 <vaitilingomvincent@gmail.com>
2024-03-27 13:58:23 +0100

    Etapes 2 & 3

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ console.log("Hello World");
 //let motUtilisateur = prompt("Entrez un mot :")
 //console.log(motUtilisateur)
 
-
 //Deuxième partie
 // const motApplication = "Bonjour"
 // let motUtilisateur = prompt("Entrez le mot : " + motApplication)
@@ -15,21 +14,25 @@ console.log("Hello World");
 //     console.log("Vous avez fait une erreur de frappe.")
 // }
 
-
 //Troisième partie
-const listeMots = ["Cachalot", "Pétunia" , "Serviette"];
+const listeMots = ["Cachalot", "Pétunia", "Serviette"];
 
 let score = 0;
 
 let motUtilisateur = prompt("Entrez le mot : " + listeMots[0]);
 
-if (motUtilisateur === listeMots[0]&& motUtilisateur === listeMots[1] && motUtilisateur === listeMots[2]) {
-    console.log("Bravo !");
+if (motUtilisateur === listeMots[0]) {
+  score++;
+
+  motUtilisateur = prompt("Entrez le mot : " + listeMots[1]);
+  if (motUtilisateur === listeMots[1]) {
     score++;
-} else {
-    console.log("Vous avez fait une erreur de frappe.")
+
+    motUtilisateur = prompt("Entrez le mot : " + listeMots[2]);
+    if (motUtilisateur === listeMots[2]) {
+      score++;
+    }
+
+    console.log("Votre score est de : " + score + "/3");
+  }
 }
-
-
-console.log("Votre score est de : " + score);
-
