35ea7f61f6753290ee8b35737cc5aa274e7e8aee
der411 <vaitilingomvincent@gmail.com>
2024-03-28 13:21:43 +0100

    Etapes 1 Les fonctions

--- script.js
+++ script.js
@@ -55,42 +55,83 @@ console.log("Hello World");
 
 // console.log("Votre score est de : " + score + "/" + listeMots.length);
 
-
 //Etape 2
+
+// const listeMots = ["Cachalot", "Pétunia", "Serviette"];
+// const listePhrases = ["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le poisson"];
+// let score = 0;
+
+// // Déclaration de la variable contenant le choix de l'utilisateur
+// let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+// // Tant que l'utilisateur n'a pas saisi "mots" ou "phrases", on lui redemande de saisir un choix
+// while (choix !== "mots" && choix !== "phrases") {
+//     choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+
+//     if (choix === "mots") {
+//         // On parcourt le tableau des mots
+//         for (let i = 0; i < listeMots.length; i++) {
+//             // On demande à l'utilisateur de saisir le mot correspondant à l'indice i
+//             let motUtilisateur = prompt("Entrez le mot : " + listeMots[i])
+//             if (motUtilisateur === listeMots[i]) {
+//                 // Si le mot saisi par l'utilisateur est correct, on incrémente le score
+//                 score++
+//             }
+//         }
+//         console.log("Votre score est de " + score + " sur " + listeMots.length)
+//     } else {
+//         // On parcourt le tableau des phrases
+//         for (let i = 0; i < listePhrases.length; i++) {
+//             // On demande à l'utilisateur de saisir la phrase correspondant à l'indice i
+//             let phraseUtilisateur = prompt("Entrez la phrase : " + listePhrases[i])
+//             if (phraseUtilisateur === listePhrases[i]) {
+//                 // Si la phrase saisi par l'utilisateur est correct, on incrémente le score
+//                 score++
+//             }
+//         }
+//         console.log("Votre score est de " + score + " sur " + listePhrases.length)
+//     }
+// }
+
+//*********************************************Les fonctions************************************//
 
 const listeMots = ["Cachalot", "Pétunia", "Serviette"];
 const listePhrases = ["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le poisson"];
-let score = 0;
 
-// Déclaration de la variable contenant le choix de l'utilisateur
-let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
-// Tant que l'utilisateur n'a pas saisi "mots" ou "phrases", on lui redemande de saisir un choix
-while (choix !== "mots" && choix !== "phrases") {
-    choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+function afficherResultat(score, nbMotsProposes) {
+  console.log("Votre score est de " + score + " sur " + nbMotsProposes);
+}
 
-    if (choix === "mots") {
-        // On parcourt le tableau des mots
-        for (let i = 0; i < listeMots.length; i++) {
-            // On demande à l'utilisateur de saisir le mot correspondant à l'indice i
-            let motUtilisateur = prompt("Entrez le mot : " + listeMots[i])
-            if (motUtilisateur === listeMots[i]) {
-                // Si le mot saisi par l'utilisateur est correct, on incrémente le score
-                score++
-            }
-        }
-        console.log("Votre score est de " + score + " sur " + listeMots.length)
-    } else {
-        // On parcourt le tableau des phrases
-        for (let i = 0; i < listePhrases.length; i++) {
-            // On demande à l'utilisateur de saisir la phrase correspondant à l'indice i
-            let phraseUtilisateur = prompt("Entrez la phrase : " + listePhrases[i])
-            if (phraseUtilisateur === listePhrases[i]) {
-                // Si la phrase saisi par l'utilisateur est correct, on incrémente le score
-                score++
-            }
-        }
-        console.log("Votre score est de " + score + " sur " + listePhrases.length)
+function choisirPhraseOuMot() {
+  let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?");
+  while (choix !== "mots" && choix !== "phrases") {
+    choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?");
+  }
+  return choix;
+}
+
+function lancerBoucleDeJeu(listePropositions) {
+  let score = 0;
+  for (let i = 0; i < listePropositions.length; i++) {
+    motUtilisateur = prompt("Entrez le mot ou la phrase : " + listePropositions[i]);
+    if (motUtilisateur === listePropositions[i]) {
+      score++;
     }
-}    
+  }
+  return score;
+}
 
-//*********************************************Les fonctions************************************//
+function lancerJeu() {
+  let choix = choisirPhraseOuMot();
+  let score = 0;
+  let nbMotsProposes = 0;
+  if (choix === "mots") {
+    score = lancerBoucleDeJeu(listeMots);
+    nbMotsProposes = listeMots.length;
+  } else {
+    score = lancerBoucleDeJeu(listePhrases);
+    nbMotsProposes = listePhrases.length;
+  }
+  afficherResultat(score, nbMotsProposes);
+}
+
+lancerJeu();
